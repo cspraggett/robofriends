ac2e6272a91a017f8e072599c825a1f03d605fa8
cspraggett <cspraggett@gmail.com>
2021-03-19 11:57:54 -0400

    Add redux for state managment

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,33 +1,55 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-function App() {
-	const [robots, setRobots] = useState([]);
-	const [searchfield, setSearchfield] = useState('');
+import { setSearchField, requestRobots } from '../actions';
+
+const mapStateToProps = (state) => {
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error,
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots),
+	};
+};
+
+function App(props) {
+	// const [robots, setRobots] = useState([]);
+	// const [searchfield, setSearchfield] = useState('');
 	const [count, setCount] = useState(0);
 
+	const { searchField, onSearchChange, robots, isPending } = props;
+
 	useEffect(() => {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then((response) => response.json())
-			.then((users) => {
-				setRobots(users.map(({ id, name, email }) => ({ id, name, email })));
-			})
-			.catch((err) => console.error(err));
+		props.onRequestRobots();
+		// fetch('https://jsonplaceholder.typicode.com/users')
+		// 	.then((response) => response.json())
+		// 	.then((users) => {
+		// 		setRobots(users.map(({ id, name, email }) => ({ id, name, email })));
+		// 	})
+		// 	.catch((err) => console.error(err));
 	}, []);
 
-	const onSearchChange = (event) => {
-		setSearchfield(event.target.value);
-	};
+	// const onSearchChange = (event) => {
+	// 	setSearchfield(event.target.value);
+	// };
 
 	const filteredRobots = robots.filter((robot) =>
-		robot.name.toLowerCase().includes(searchfield.toLowerCase())
+		robot.name.toLowerCase().includes(searchField.toLowerCase())
 	);
 
-	return !robots.length ? (
+	return isPending ? (
 		<h1>Loading</h1>
 	) : (
 		<div className="tc">
@@ -43,4 +65,4 @@ function App() {
 	);
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
